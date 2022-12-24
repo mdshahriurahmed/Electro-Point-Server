4fb31fbf2dce61c085c82efc568ff436a9236400
mdshahriurahmed <shahriurahmedcse@gmail.com>
2022-12-24 09:33:55 +0600

    aa

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const inventorycollection = client.db("electro-point").collection("Inventories");
 
 
